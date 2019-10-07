64b3a947a848cca4b4a87d451a9bd2329399381b
Michael Widlok <michaelwidlok@yahoo.de>
2019-10-07 18:34:55 +0200

    startet with creating the repository

--- app/src/main/java/com/raywenderlich/android/creaturemon/model/Creature.kt
+++ app/src/main/java/com/raywenderlich/android/creaturemon/model/Creature.kt
@@ -30,10 +30,15 @@
 
 package com.raywenderlich.android.creaturemon.model
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import android.support.annotation.NonNull
+
+@Entity(tableName = "creature_table")
 data class Creature(
         val attributes:CreatureAttributes = CreatureAttributes(),
         val hitpoints:Int = 0,
-        val name:String = "",
+        @PrimaryKey @NonNull val name:String = "",
         val drawable:Int = 0
 
 )
--- app/src/main/java/com/raywenderlich/android/creaturemon/model/CreatureRepository.kt
+++ app/src/main/java/com/raywenderlich/android/creaturemon/model/CreatureRepository.kt
@@ -30,4 +30,11 @@
 
 package com.raywenderlich.android.creaturemon.model
 
+import android.arch.lifecycle.LiveData
+
 interface CreatureRepository
+{
+    fun saveCreature(creature:Creature)
+    fun getAllCreatures():LiveData<List<Creature>>
+    fun clearAllCreatures()
+}
